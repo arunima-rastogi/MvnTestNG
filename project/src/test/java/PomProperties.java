8ae2e48c04e80c7b92592ebf49a65fd2a6e07b12
Rastogi <srasz@allstate.com>
2020-01-17 06:15:24 -0500

    read pom properties in java code

---
+++ project/src/test/java/PomProperties.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Properties;
+
+public class PomProperties {
+
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
+		Class cls = Class.forName("PomProperties");
+		ClassLoader cLoader = cls.getClassLoader();
+		java.io.InputStream is = cls.getResourceAsStream("my.properties");
+		java.util.Properties p = new Properties();
+		p.load(is);
+		String name = p.getProperty("name");
+		String version = p.getProperty("version");
+		String foo = p.getProperty("foo");
+
+		System.out.println(name + version + foo);
+	}
+
+}
